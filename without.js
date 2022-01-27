0086c94b325a99618144356533f745e581f07f52
Robert812218 <robert812@shaw.ca>
2022-01-27 22:24:09 +0000

    remove item/items from array

---
+++ without.js
@@ -0,0 +1,23 @@
+const without = function(source, itemsToRemove) {
+  let arr = [];
+
+  let sweeperoni = function(item, scan) {
+    for (const j of scan) {
+      if (item === j) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for (const i of source) {
+    if (sweeperoni(i, itemsToRemove)) {
+      arr.push([i]);
+    }
+  }
+  return arr;
+}
+
+console.log(without([1, 2, 3], [1]));
+console.log(without(["1", "2", "3"], [1, 2, "3"]));
+console.log(without(["yuh", "yuh", 55, 100], ["yuh", "torque", 55, 200]));
