2490b5bbd3cf8581dce42031c7a3cf3e6091b152
Robert812218 <robert812@shaw.ca>
2022-01-28 17:55:12 +0000

    return middle of array

--- middle.js
+++ middle.js
@@ -39,14 +39,12 @@ const middle = function(arr) {
   }
 };
 
-// The middle function should return an array with only the middle element(s) of the provided array. This means that the length of the returned elements could vary.
 
-// For arrays with one or two elements, there is no middle. Return an empty array.
 console.log(middle([1])); // => []
 console.log(middle([1, 2])); // => []
-// For arrays with odd number of elements, an array containing a single middle element should be returned.
+
 console.log(middle([1, 2, 3])); // => [2]
 console.log(middle([1, 2, 3, 4, 5])); // => [3]
-// For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+
 console.log(middle([1, 2, 3, 4])); // => [2, 3]
 console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
