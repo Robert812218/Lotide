27db99503bd55b54084de03e154452fe753a6bab
Robert812218 <robert812@shaw.ca>
2022-02-20 20:42:32 +0000

    added .gitignore file, also cleaned up dead code etc in other files

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,31 +1,5 @@
-// const eqArrays = require('./eqArrays');
-
-const assertEqual = function(actual, expected) {
-  const emoji = require('node-emoji');
-  const go = emoji.get("white_check_mark");
-  const nicht = emoji.get("question");
-
-  if (actual === expected) {
-    console.log(`${go}${go}${go} Assertion Passed: ${actual} === ${expected}`);
-    return true;
-  } else if (actual !== expected) {
-    console.log(`${nicht}${nicht}${nicht} Assertion Failed: ${actual} !== ${expected}`);
-    return false;
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  let boolsonn = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (!assertEqual(arr1[i], arr2[i])) {
-      return false;
-    }
-  }
-
-  if (boolsonn) {
-    return true;    
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
 
 const assertArraysEqual = function(arr1, arr2) {
   if (!eqArrays(arr1, arr2)) {
--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,39 +1,7 @@
 const check = require('check-node-version');
-
-const assertEqual = function(actual, expected) {
-  const emoji = require('node-emoji');
-  const go = emoji.get("white_check_mark");
-  const nicht = emoji.get("question");
-
-  if (actual === expected) {
-    console.log(`${go}${go}${go} Assertion Passed: ${actual} === ${expected}`);
-    return true;
-  } else if (actual !== expected) {
-    console.log(`${nicht}${nicht}${nicht} Assertion Failed: ${actual} !== ${expected}`);
-    return false;
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  let boolsonn = true;
-  for (let i = 0; i < arr1.length; i++) {
-    if (!assertEqual(arr1[i], arr2[i])) {
-      return false;
-    }
-  }
-
-  if (boolsonn) {
-    return true;    
-  }
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (!eqArrays(arr1, arr2)) {
-    return "Arrays are not equal.";
-  } else {
-    return "Can confirm, these arrays are equal.";
-  }
-};
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const assertObjectsEqual = function(object1, object2) {
@@ -57,10 +25,6 @@ const assertObjectsEqual = function(object1, object2) {
   kArr2.sort();
   vArr2.sort();
 
-  // console.log(kArr1);
-  // console.log(kArr2);
-  // console.log(vArr1);
-  // console.log(vArr2);
 
   if (kArr1.length !== kArr2.length || vArr1.length !== vArr2.length) {
     return false;
@@ -85,10 +49,9 @@ const assertObjectsEqual = function(object1, object2) {
   }
 
   if (assertEqual(checkKeys(kArr1, kArr2), checkVals(vArr1, vArr2))) {
-    // return `True: keys, \n${kArr1} \nVals:\n ${vArr2}`;
-    return `True: ${object1} === ${object2}`;
+    return true;
   } else {
-    return `False. ${object1} !== ${object2}`;
+    return false;
   }
 }
 
