b41cdfdf305d276e90722d9268363e496b6e8faa
Robert812218 <robert812@shaw.ca>
2022-03-07 19:39:33 +0000

    remove unused imports

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,9 @@
 const assertEqual = function(actual, expected) {
-  const emoji = require('node-emoji');
-  const go = emoji.get("white_check_mark");
-  const nicht = emoji.get("question");
 
   if (actual === expected) {
-    console.log(`${go}${go}${go} Assertion Passed: ${actual} === ${expected}`);
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log(`${nicht}${nicht}${nicht} Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
