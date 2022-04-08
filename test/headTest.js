1a2461e47b5f39391220d33f53de34eb379c5aaa
Robert812218 <robert812@shaw.ca>
2022-04-08 17:01:54 +0000

    fix functional requirements

--- test/headTest.js
+++ test/headTest.js
@@ -5,15 +5,15 @@ const { assert } = require('chai');
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
-  })
+  });
 
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5');
   });
 });
 
 
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head(["TORQUE", "TRON", "GUACAMOLE"]), 999999999);
-// assertEqual(head([]), 10);
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["TORQUE", "TRON", "GUACAMOLE"]), 999999999);
+assertEqual(head([]), 10);
--- test/mapTest.js
+++ test/mapTest.js
@@ -1,8 +1,13 @@
 const map = require('../map');
 
+const words = ["ground", "control", "to", "major", "tom"];
+const items = ["item1", "item2", "item3", "item4", "item5"];
+const chunks = ["chunk1", "chunk2", "chunk3", "chunk4", "chunk5"];
+
 const results1 = map(words, word => word[0]);
 const results2 = map(items, item => item > 2);
 const results3 = map(chunks, chunk => chunk !== 0);
+
 console.log(results1);
 console.log(results2);
 console.log(results3);
